3627a66cab554546b6524abba92d89cf4672e478
rishikesh-itobuz <rishikesh@itobuz.com>
2024-04-08 11:54:32 +0530

    fix: SPOON-2373 first commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,24 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'validate-commit-message': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'validate-commit-message': ({ subject }) => {
+          if (/^SPOON-\d+\s.*$/.test(subject)) {
+            return [true];
+          }
+          return [
+            false,
+            `Please provide a valid Commit message pattern : '<type>: [SPOON-<ISSUE_ID>] <commit-message>'.`,
+          ];
+        },
+      },
+    },
+  ],
+};
